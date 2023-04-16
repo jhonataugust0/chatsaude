712a2fd77dfa82078f7fa36972c95431c4145509
Jhonata Augusto <jhon.augustosilva@gmail.com>
2023-04-16 18:15:34 -0300

    Efetuação da send_message assíncrona

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,12 +1,16 @@
 from typing import Generator
-from fastapi.testclient import TestClient
 import pytest
+import os
+import sys
+from dotenv import load_dotenv
+from fastapi.testclient import TestClient
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from main import app
 
-from dotenv import load_dotenv
 
 def pytest_load_initial_conftests(early_config, parser, args):
     load_dotenv()
+
 
 @pytest.fixture(scope="class")
 def client() -> Generator:
