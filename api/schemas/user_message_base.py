299e917dce1bad0b726887bbdf5d7dd56c814f1e
JhonataAugust0 <jhon.augustosilva@gmail.com>
2023-06-23 15:50:44 -0300

    Schema

--- api/schemas/user_message_base.py
+++ api/schemas/user_message_base.py
@@ -1,7 +1,7 @@
 from dataclasses import Field
 import os
 from typing import Dict, List
-from pydantic import BaseModel, validator, root_validator
+from pydantic import BaseModel, validator, root_validator, fields
 from datetime import datetime
 from datetime import date as date_type
 from datetime import time as time_type
@@ -34,11 +34,25 @@ class Message(BaseModel):
     category: str
     value: str
 
-    @validator('value')
+    @root_validator(pre=True)
+    def validate_field(cls, values):
+        types_validators = {
+            'message': lambda value: cls.validate_value(value),
+            # 'cpf': validate_cpf,
+            # 'email': validate_email
+        }
+
+        if values['category'] in types_validators:
+            validator_func = types_validators.get(values['category'])
+            validator_func(values['value'])
+            return values['value']
+
+    @classmethod
     def validate_value(cls, value):
         if not isinstance(value, str):
             raise HTTPException(status_code=400, detail='O campo "value" deve ser uma string.')
         return value
+
 
 class Return(BaseModel):
     category: str
